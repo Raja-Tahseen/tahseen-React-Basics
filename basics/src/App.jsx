ab23baf39d251188527abd5ca34daea04b2ff11a
Raja Tahseen <rajatahseen86@outlook.com>
2025-04-29 06:31:16 +0500

    Javascript Counter

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+
+const App = () => {
+  //const [counter, setCounter] = useState(0);
+  let counter = 0;
+
+  const handleClick1 = () => {
+    //setCounter(counter + 1);
+    counter++;
+    console.log(counter);
+  };
+
+  const handleClick2 = () => {
+    //setCounter(counter - 1);
+    counter--;
+    console.log(counter);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "300%",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: "-15%",
+      }}
+    >
+      Counter App
+      <div
+        style={{
+          fontSize: "120%",
+          position: "relative",
+          top: "10vh",
+        }}
+      >
+        {counter}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="buttons">
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginRight: "5px",
+            backgroundColor: "green",
+            borderRadius: "8%",
+            color: "white",
+          }}
+          onClick={handleClick1}
+        >
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          style={{
+            fontSize: "60%",
+            position: "relative",
+            top: "20vh",
+            marginLeft: "5px",
+            backgroundColor: "red",
+            borderRadius: "8%",
+            color: "white",
+          }}
+          onClick={handleClick2}
+        >
+          Decrement
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
